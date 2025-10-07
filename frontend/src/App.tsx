1f84530f49ae3451e480adfb6fca95adaa1ce3db
Muhammad-Ahmed94 <muneebbum757@gmail.com>
2025-10-07 16:33:10 +0500

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div className='text-red-500 bg-blue-600 bold text-2xl'>App</div>
+  )
+}
+
+export default App
